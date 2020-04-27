f7bf3422cfe7f7b3a2853150c5127bfca8098efa
Kseg97 <camilosegura6@gmail.com>
2020-04-27 08:00:24 -0500

    Added Charts

--- expo_ibm_comm/App.js
+++ expo_ibm_comm/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LineChart, YAxis, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 
 async function getData() {
   const db = "ventilator_db";
@@ -36,20 +38,57 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      data: "",
+      data: [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }],
     };
   }
-  componentDidMount() {
-    getData()
-      .then(data => this.setState({ data: data.docs }))
-      .catch((err) => console.error(err))
 
+  componentDidMount() {
+    this.dataInterval = setInterval(() => getData()
+      .then(data => this.setState({ data: data.docs || this.state.data }))
+      .catch((err) => console.error(err))
+      , 3000)
   }
+
+  componentWillUnmount() {
+    clearInterval(this.dataInterval);
+  }
+
   render() {
+    const data = this.state.data.map(item => item.value);//[50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+    const contentInset = { top: 20, bottom: 20 }
+
     return (
       <View style={styles.container}>
         <Text>Gotcha!</Text>
-        <Text>{JSON.stringify(this.state.data)}</Text>
+        {/* <Text>{JSON.stringify(this.state.data)}</Text> */}
+        <View style={{ height: '60%', width: '100%', flexDirection: 'row' }}>
+          <YAxis
+            data={data}
+            contentInset={contentInset}
+            svg={{
+              fill: 'grey',
+              fontSize: 10,
+            }}
+            numberOfTicks={10}
+            formatLabel={(value) => `${value} ppm`}
+          />
+          <LineChart
+            style={{ width: '80%', marginLeft: 16 }}
+            data={data}
+            svg={{ stroke: 'rgb(134, 65, 244)', strokeWidth: 3 }}
+            contentInset={contentInset}
+            curve={shape.curveNatural}
+          >
+            <Grid />
+          </LineChart>
+          {/* <XAxis
+                    style={{ marginHorizontal: -10 }}
+                    data={data}
+                    formatLabel={(value, index) => index}
+                    contentInset={{ left: 10, right: 10 }}
+                    svg={{ fontSize: 10, fill: 'black' }}
+                /> */}
+        </View>
       </View>
     )
   }
