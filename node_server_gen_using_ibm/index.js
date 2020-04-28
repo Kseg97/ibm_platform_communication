06a3319fdc1eff7648ecc3a30a20010329308f2c
Kseg97 <camilosegura6@gmail.com>
2020-04-28 00:07:25 -0500

    Added support for CSV. Signal file included. Changes on node server.

--- node_server_gen_using_ibm/index.js
+++ node_server_gen_using_ibm/index.js
@@ -1,21 +1,60 @@
 const cloudant = require('./lib/cloudant.js');
+const neatCsv = require('neat-csv');
+const fs = require('fs')
+
+var data;
+
+fs.readFile('./signal.csv', async (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    // specific treatment depending on CSV
+    someData = await neatCsv(data);
+    data = Object.keys(someData[0]).map(number => +number);
+
+    var iter = 0;
+    var value = 0;
+    setTimeout(function () {
+        setInterval(function () {
+            try {
+                value = data[iter];
+                console.log(`${iter}:${value}`);
+                iter++;
+                if (iter >= data.length) iter = 0
+
+                cloudant
+                    .create('someType', value, 'someUserID')
+                    .then(data => {
+                        if (data.statusCode != 201) {
+                            console.log(data.statusCode)
+                        } else {
+                            console.log(data.data)
+                        }
+                    })
+                    .catch(err => console.error(err));
+            } catch (e) { }
+        }, 250); // Interval to emulate the sensor update.
+    }, 5000); // Timeout to initialize the DB connection. TODO: improve the connector to be async.
+})
+
 /* TEST */
-setTimeout(function () {
-    setInterval(function () {
-        try {
-            cloudant
-                .create('someType', Math.random(), 'someUserID')
-                .then(data => {
-                    if (data.statusCode != 201) {
-                        console.log(data.statusCode)
-                    } else {
-                        console.log(data.data)
-                    }
-                })
-                .catch(err => console.error(err));
-        } catch (e) { }
-    }, 250); // Interval to emulate the sensor update.
-}, 5000); // Timeout to initialize the DB connection. TODO: improve the connector to be async.
+// setTimeout(function () {
+//     setInterval(function () {
+//         try {
+//             cloudant
+//                 .create('someType', Math.random(), 'someUserID')
+//                 .then(data => {
+//                     if (data.statusCode != 201) {
+//                         console.log(data.statusCode)
+//                     } else {
+//                         console.log(data.data)
+//                     }
+//                 })
+//                 .catch(err => console.error(err));
+//         } catch (e) { }
+//     }, 250); // Interval to emulate the sensor update.
+// }, 5000); // Timeout to initialize the DB connection. TODO: improve the connector to be async.
 
 /**
  * Get a list of resources
